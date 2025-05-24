65a89c8d308e6c12aad865088c2619c2348ff631
Sanny Sanoff <san@sysdate.com>
2025-05-24 08:54:58 +0200

    fix: remove hardcoded ezETH rewards points.

--- internal/scraper/ezeth.go
+++ internal/scraper/ezeth.go
@@ -1,8 +1,8 @@
 package scraper
 
 import (
-	"log"
 	"example.com/rates/v2/internal/model"
+	"log"
 )
 
 const (
@@ -14,7 +14,7 @@ const (
 
 func ScrapeEzETH() []model.Rate {
 	log.Println("[scraper] Scraping (hardcoded) ezETH rates...")
-	points := "2x Zircuit Points; 2x Mellow Points; 3x InceptionLRT Totems; 1x Symbiotic Points"
+	points := "2x Zircuit Points"
 	return []model.Rate{
 		{
 			InputSymbol: "ETH",
